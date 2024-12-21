47478057e9b91ba071afd0f3dd37c98a627d3e78
muhammad sheraz <msd.sheraz046@gmail.com>
2024-12-21 09:43:14 +0500

    updating

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const mainRouter = require('./routes/mainRouter');
 const ownerRouter = require('./routes/ownersRouter');
 const cookieParser = require('cookie-parser');
+const ejs = require('ejs')
 const app = express();
 const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const serverless = require('serverless-http');
 const flash = require('connect-flash');
 require('./config/mongooseConfiguration');
 require('dotenv').config();
@@ -15,10 +14,6 @@ app.use(session({
   secret: `${process.env.JWT_KEY}`,
   resave: false,
   saveUninitialized: true,
-  store: MongoStore.create({
-      mongoUrl: `${process.env.DB_URI}`,
-      collectionName: 'sessions',
-  }),
 }));
 app.use(flash());
 app.use(express.urlencoded({extended : true}));
@@ -29,4 +24,3 @@ app.use("/" , mainRouter);
 app.use("/owner" , ownerRouter);
 
 app.listen(3000);
-module.exports = serverless(app)
