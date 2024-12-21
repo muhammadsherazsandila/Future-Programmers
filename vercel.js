7530c392ad1a9851b45702adb980af942ff7101f
muhammad sheraz <msd.sheraz046@gmail.com>
2024-12-21 09:10:46 +0500

    fixing

--- vercel.js
+++ vercel.js
@@ -1,4 +1,6 @@
 const serverless = require('serverless-http');
 const app = require('./index'); // Adjust the path to your Express app
 
-module.exports = serverless(app);
+module.exports = serverless(app , {
+    framework: 'express' // Explicitly specify the framework
+});
