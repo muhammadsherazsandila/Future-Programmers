bcad2d6ebfa6c4a3f138a65e4870e59ec7116ed7
muhammad sheraz <msd.sheraz046@gmail.com>
2024-12-21 10:04:05 +0500

    updating

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(session({
 }));
 app.use(flash());
 app.use(express.urlencoded({extended : true}));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname , "public")));
 app.use(express.json());
 app.use(cookieParser())
 app.use("/" , mainRouter);
