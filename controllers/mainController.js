d7e5f26b9ef380f1dda06be908d360554559d0a3
muhammad sheraz <msd.sheraz046@gmail.com>
2024-12-21 10:01:10 +0500

    updating

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -19,6 +19,7 @@ const createProgram = async (req, res) => {
     })
     owner.programs.push(program._id);
     owner.save();
+    req.flash("error_msg" , "Uploaded Successfully!")
     res.redirect("/loggedInOwner")
 }
 const adminPage = async (req, res) => {
