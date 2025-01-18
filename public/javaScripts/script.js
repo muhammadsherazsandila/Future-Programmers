4b47b36a9674ee65efd61f1df0c83c3573c95c4f
Muhammad Sheraz <msd.sheraz046@gmail.com>
2025-01-19 00:18:00 +0500

    adding Mouse Evnet

--- public/javaScripts/script.js
+++ public/javaScripts/script.js
@@ -7,3 +7,16 @@ if (alert.innerText !== '') {
         alert.style.opacity = '0'
     }, 2000);
 }
+
+document.addEventListener('click', (event) => {
+    const ripple = document.createElement('div');
+    ripple.classList.add('ripple');
+    ripple.style.left = `${event.clientX - 10}px`;
+    ripple.style.top = `${event.clientY - 10}px`;
+    document.body.appendChild(ripple);
+
+    // Remove the ripple after animation
+    ripple.addEventListener('animationend', () => {
+      ripple.remove();
+    });
+  });
