0e6420cbdc325ad051ab1df8854d43b4524d700f
Muhammad Sheraz <msd.sheraz046@gmail.com>
2025-02-10 21:06:57 +0500

    updating

--- models/ownerModel.js
+++ models/ownerModel.js
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose');
 const fs = require('fs');
+const { timeStamp } = require('console');
 const ownerShema = mongoose.Schema({
-    name : String,
-    email : String,
-    password : String,
-    profilePic : {
-        type : Buffer,
-        default : ()=>{
+    name: String,
+    email: String,
+    password: String,
+    profilePic: {
+        type: Buffer,
+        default: () => {
             return fs.readFileSync('./public/images/profilePlaceholder.jpg')
         }
     },
-    programs : [{
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "program"
+    programs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "program"
     }],
-    verificationCode : String
-})
+    verificationCode: String
+}, { timeStamps: true })
 
-module.exports = mongoose.model("owner" , ownerShema)
+module.exports = mongoose.model("owner", ownerShema)
--- models/programModel.js
+++ models/programModel.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 let programShema = mongoose.Schema({
-    title : String,
-    code : String,
-    ownerId : {
-        type : mongoose.Schema.Types.ObjectId,
-        reg : 'owner'
+    title: String,
+    code: String,
+    ownerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        reg: 'owner'
     }
-})
-module.exports = mongoose.model("program" , programShema)
+}, { timeStamps: true })
+module.exports = mongoose.model("program", programShema)
